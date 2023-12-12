1086c617c4a9d71c42ecbced9919c466124c505f
ryangosling07 <145987488+ryangosling07@users.noreply.github.com>
2023-12-12 20:40:02 +0600

    п

--- the_string/lib/home_page.dart
+++ the_string/lib/home_page.dart
@@ -109,7 +109,7 @@ gfdgfdgfdgfdhgfutrwb grde vfd gfd
  uguygugyuii gyugyuiyuyuguigyu
  gyugyvfugfyugyguyguy gyugiugyugyu
  gfdgfdgfdgdf
- gfdgfdgfduhfdsh gdhsuighidasgidfgftyftyftycxdxfkjhkjhhgjhgxgfxgfftygtgtgtyhui
+ gfdgfdgfduhfdsh gdhsuighidasgidfgftyftyftycxdxfkjhkjhhgjhgxgfxgfпнпгftygtgtgtyhui
  fgdjghfdjihgdfugifd
  gfdgfdgfdagjifoajijoi jiojoijoijiojoijoijoij nvnfd
  vfdhtdmysetregfdgh ali  gay gfdiogiotmimgfd 
